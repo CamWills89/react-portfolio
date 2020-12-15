28b0f6b318664c5c245ed0ea038f23893536228b
Cameron <cameron.wills89@outlook.com>
2020-12-15 14:12:33 -0500

    setup colour scheme added favicon  and imported some ccs

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="my-5">Hello, this is my Header</h1>
+    </header>
+  );
+}
+
+export default Header;
