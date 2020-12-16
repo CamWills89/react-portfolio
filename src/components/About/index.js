6cbfae5e5aba552142ebfe6b9781a8b8402673ad
Cameron <cameron.wills89@outlook.com>
2020-12-16 12:12:03 -0500

    added About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,21 @@
 import React from "react";
+import profileImage from "../../assets/profile/Profile.JPG";
 
 function About() {
   return (
     <section>
       <h2>About Me</h2>
-      <div class="about__content">
-        <div class="about__text">
+      <div>
+        <div>
           <p>
-            My name is Cameron Wills. I am currently a bootcamp student,
-            learning the art of web development. I am from South Africa, living
-            in Atlanta, GA, USA. The purpose of this portfolio is to display my
-            personal bio to whom ever is interested in me for professional
-            reasons. This will hopefully demonstrate who I am as a professional
-            web developer. It is meant to demonstrate deployed apps/work that I
-            have previously done, my Curriculum Vitae and how to get in contact
-            with me on multiple different platforms.
+            My name is Cameron Wills. I am a Fullstack web developer. I am from
+            South Africa, living in Atlanta, GA, USA. The purpose of this
+            portfolio is to display my personal bio to whom ever is interested
+            in me for professional reasons. This will hopefully demonstrate who
+            I am as a professional web developer. This will demonstrate deployed
+            apps/work that I have previously done, my Curriculum Vitae and how
+            to get in contact with me on multiple different platforms.
           </p>
-
           <a
             href="https://drive.google.com/file/d/1eL7QXO2qeXAxT0zsOQHMCzylRCdyD-Am/view?usp=sharing"
             target="_blank"
@@ -25,9 +24,8 @@ function About() {
             My Resume
           </a>
         </div>
-
-        <div class="about__photo-container">
-          <img class="about__photo" src="./images/Profile.JPG" alt="" />
+        <div>
+          <img src={profileImage} className="my-2" alt="profile image" />
         </div>
       </div>
     </section>
