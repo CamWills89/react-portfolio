4c83c456b862ecd8b8b643a888c0ea704bdcb9e3
Cameron <cameron.wills89@outlook.com>
2020-12-18 14:33:51 -0500

    Updates

---
+++ precache-manifest.607942f47efa5a70d9532ee9c1186231.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26747270d17923afc172cf5d12243577",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4fd513c5bab2c0629e8a",
+    "url": "/react-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6a2bf20374bdfc8b7328",
+    "url": "/react-portfolio/static/css/main.aedf43d5.chunk.css"
+  },
+  {
+    "revision": "4fd513c5bab2c0629e8a",
+    "url": "/react-portfolio/static/js/2.232eecd0.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-portfolio/static/js/2.232eecd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4284f1ced80735af2676",
+    "url": "/react-portfolio/static/js/3.00d36ae2.chunk.js"
+  },
+  {
+    "revision": "6a2bf20374bdfc8b7328",
+    "url": "/react-portfolio/static/js/main.3aefecd1.chunk.js"
+  },
+  {
+    "revision": "74b3a38a9fa1d57002d3",
+    "url": "/react-portfolio/static/js/runtime-main.bbb2feee.js"
+  },
+  {
+    "revision": "a1ce555c7fac205e829b2c7d718b1403",
+    "url": "/react-portfolio/static/media/Background.a1ce555c.jpg"
+  },
+  {
+    "revision": "6cc7e8cac0348598158710b853861925",
+    "url": "/react-portfolio/static/media/Profile.6cc7e8ca.JPG"
+  },
+  {
+    "revision": "41d993fe1b5887450357df1749993aa2",
+    "url": "/react-portfolio/static/media/auto-tracker.41d993fe.PNG"
+  },
+  {
+    "revision": "1298b87d3f96b6d69b57ba14881c3ef4",
+    "url": "/react-portfolio/static/media/budget-tracker.1298b87d.PNG"
+  },
+  {
+    "revision": "d4a4aaba297091a55161a6f6d682be9f",
+    "url": "/react-portfolio/static/media/citizen-portal.d4a4aaba.PNG"
+  },
+  {
+    "revision": "615454750e7bee3739081e5962c7c674",
+    "url": "/react-portfolio/static/media/github.61545475.svg"
+  },
+  {
+    "revision": "7c356c52b09febac972561319157fde1",
+    "url": "/react-portfolio/static/media/github2.7c356c52.png"
+  },
+  {
+    "revision": "d99c517b75458108e7747cde913d6069",
+    "url": "/react-portfolio/static/media/phone.d99c517b.png"
+  },
+  {
+    "revision": "2f9d9764b0b848075e294306c8a2f926",
+    "url": "/react-portfolio/static/media/weather-dashboard.2f9d9764.PNG"
+  }
+]);
