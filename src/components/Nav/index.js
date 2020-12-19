ce99bd9ef6f3bb0b18452a3fdb02150636f6541f
Cameron <cameron.wills89@outlook.com>
2020-12-19 16:19:14 -0500

    refactored my navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,30 +1,3 @@
-// import React from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// function Nav(props) {
-//   const tabs = ["About", "Portfolio", "Skills", "Contact"];
-//   return (
-//     <ul className="nav">
-      
-//       {tabs.map((tab) => (
-//         <li className="" key={tab}>
-//           <a
-//             href={"#" + tab.toLowerCase()}
-//             onClick={() => props.handlePageChange(tab)}
-//             className={
-//               props.currentPage === tab ? "nav-link active" : "nav-link"
-//             }
-//           >
-//             {tab}
-//           </a>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// export default Nav;
-
 import React from 'react';
 import styled from 'styled-components';
 import Burger from './Burger';
